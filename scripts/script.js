226162ef485d02384d511c4d1fe61575c85b0af4
carlos <cfernande1408@gmail.com>
2022-12-05 16:59:22 +0100

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const selectDiv = document.querySelector('#grid_container');
+let arrayGrid = [[],[]];
+
+function grid(){
+
+
+for(let i = 0; i<256; i++){
+
+        const createP = document.createElement('div');
+    
+        selectDiv.appendChild(createP);
+    
+}
+}
+
+
+
+grid();
